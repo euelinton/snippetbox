301f0d5fd0bf208cb7e1afbbca5d765a13678938
euelinton <euelinton@outlook.com>
2025-07-17 20:37:34 -0300

    add snippetmodel latest method and modify home handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
 
@@ -11,21 +10,29 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"./ui/html/pages/base.html",
-		"./ui/html/partials/nav.html",
-		"./ui/html/pages/home.html",
-	}
-	tmpl, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
-		http.Error(w, "Erro interno no servidor", http.StatusInternalServerError)
+		return
 	}
-	err = tmpl.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, r, err)
-		http.Error(w, "Erro interno no servidor", http.StatusInternalServerError)
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+	// files := []string{
+	// 	"./ui/html/pages/base.html",
+	// 	"./ui/html/partials/nav.html",
+	// 	"./ui/html/pages/home.html",
+	// }
+	// tmpl, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, r, err)
+	// 	http.Error(w, "Erro interno no servidor", http.StatusInternalServerError)
+	// }
+	// err = tmpl.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	app.serverError(w, r, err)
+	// 	http.Error(w, "Erro interno no servidor", http.StatusInternalServerError)
+	// }
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
